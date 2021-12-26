8e41b2350d900e8db8658e41459df8669c15d173
shubhamstr <shubham.sutar@vinnovatetechnologies.com>
2021-12-26 22:44:08 +0530

    update home

--- index.php
+++ index.php
@@ -28,7 +28,9 @@
 
 
 <!-- Latest compiled JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
 <script>
     function register(params) {
