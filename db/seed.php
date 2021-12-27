a749f4d7a0514c0c5f5e399fa411d65fc2e5fc1a
shubhamstr <shubham.sutar@vinnovatetechnologies.com>
2021-12-27 11:25:44 +0530

    seed update

--- db/seed.php
+++ db/seed.php
@@ -1,6 +1,5 @@
 <?php
-
-$con = mysqli_connect("localhost", "root", "");
+include './config.php'; 
 
 
 
@@ -8,18 +7,24 @@ $users_sql1 = "CREATE DATABASE `login_system2`;";
 $users_result1 = mysqli_query($con,$users_sql1);
 if($users_result1){
     echo "DATABASE login_system2 Created"."<br>";
+}else{
+    echo "DATABASE login_system2 Create issue"."<br>";
 }
 
 $users_sql2 = "CREATE TABLE `login_system2`.`users` ( `id` INT NOT NULL AUTO_INCREMENT ,  `username` VARCHAR(250) NOT NULL ,  `email` VARCHAR(250) NOT NULL ,  `password` VARCHAR(250) NOT NULL ,  `cpassword` VARCHAR(250) NOT NULL ,  `token` VARCHAR(250) NOT NULL ,  `created_at` TIMESTAMP NOT NULL ,    PRIMARY KEY  (`id`))";
 $users_result2 = mysqli_query($con,$users_sql2);
 if($users_result2){
     echo "TABLE users Created"."<br>";
+}else{
+    echo "TABLE users Create issue"."<br>";
 }
 
 $users_sql3 = "ALTER TABLE `users` ADD UNIQUE(`email`);";
 $users_result3 = mysqli_query($con,$users_sql3);
 if($users_result3){
     echo "TABLE users Updated"."<br>";
+}else{
+    echo "TABLE users Update issue"."<br>";
 }
 
                 
